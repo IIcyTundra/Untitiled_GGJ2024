249366c8b4b7a9acd32778011ed56025fcfe06e7
Garry Chen <chen.garry81611@gmail.com>
2024-01-28 13:54:35 -0800

    Updated local multiplayer control

--- Scripts/Hand.gd
+++ Scripts/Hand.gd
@@ -5,19 +5,32 @@ signal hit_wall(push_v: Vector3)
 
 const TURN_SPEED_MAX := 100 # in radians/sec
 const TURN_SPEED_MIN := 10 # in radians/sec
+const TURN_SPEED_HOLD := 5.0
+const TURN_SPEED_SWING := 15.0
 const TURN_ACC := 5 # in radians/sec
 const TURN_Thrash := 0.1 # in radians/sec
-const PUSH_HORIZONTAL_V := 15.0
+const PUSH_HORIZONTAL_V := 50.0
 const PUSH_VERTICAL_V := 5.0
 const HIT_WALL_CD := 0.2
+const SWING_DURATION := 0.25
 
 var target_angle: float = 0.0
 var ang_speed: float = 0.0
 var hit_wall_timer = 0.0
-var direction: Vector2
+var swing_duration_timer = 0.0
+#var direction: Vector2
+var direction: float = 1.0
 var is_clockwise = true
 var can_hit_wall = true
 var done = true
+var attack_r_pressed = false
+var attack_l_pressed = false
+
+enum HAND_STATE {HODING, SWING, IDLE}
+var hand_state: HAND_STATE = HAND_STATE.IDLE
+enum ACTION_STATE {HOLD_CLOCKWISE, HOLD_COUNTERCLOCKWISE, IDLE}
+var action_state: ACTION_STATE = ACTION_STATE.IDLE
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -29,28 +42,65 @@ func _process(delta):
 			can_hit_wall = true
 
 func _physics_process(delta: float):
-	if done:
+	if hand_state == HAND_STATE.IDLE: 
+		if action_state == ACTION_STATE.HOLD_CLOCKWISE or action_state == ACTION_STATE.HOLD_COUNTERCLOCKWISE:
+			hand_state = HAND_STATE.HODING
 		return
-	var old_direction = Vector2(cos(rotation.y), sin(rotation.y))
-	ang_speed += TURN_ACC * delta
-	ang_speed = clamp(ang_speed, TURN_SPEED_MIN, TURN_SPEED_MAX)
-	var diff = direction - old_direction
-	var new_direction = old_direction + diff * ang_speed * delta
-	rotation.y = new_direction.angle()
-	if diff.length() < TURN_Thrash:
-		done = true
 	
-func _set_target_dir(dir):
-	direction = dir
-	var old_direction_p = Vector2(-sin(rotation.y), cos(rotation.y))
-	var new_is_clockwise = direction.dot(old_direction_p) < 0
-	if new_is_clockwise != is_clockwise:
-		ang_speed = 0
-	is_clockwise = new_is_clockwise
-	done = false
-
+	if hand_state == HAND_STATE.HODING:
+		rotation.y = rotation.y + TURN_SPEED_HOLD * delta * direction
+	elif hand_state == HAND_STATE.SWING:
+		swing_duration_timer -= delta
+		if swing_duration_timer < 0:
+			hand_state = HAND_STATE.IDLE
+		else:
+			rotation.y = rotation.y + TURN_SPEED_SWING * delta * direction
+		
 	
+#func _set_target_dir(dir):
+	#direction = dir
+	#var old_direction_p = Vector2(-sin(rotation.y), cos(rotation.y))
+	#var new_is_clockwise = direction.dot(old_direction_p) < 0
+	#if new_is_clockwise != is_clockwise:
+		#ang_speed = 0
+	#is_clockwise = new_is_clockwise
+	#done = false
 
+func attack_right_pressed():
+	attack_r_pressed = true
+	if action_state == ACTION_STATE.IDLE:
+		action_state = ACTION_STATE.HOLD_CLOCKWISE
+		direction = 1.0
+	
+func attack_left_pressed():
+	attack_l_pressed = true
+	if action_state == ACTION_STATE.IDLE:
+		action_state = ACTION_STATE.HOLD_COUNTERCLOCKWISE
+		direction = -1.0
+
+func attack_right_released():
+	attack_r_pressed = false
+	if attack_l_pressed == true:
+		action_state = ACTION_STATE.HOLD_COUNTERCLOCKWISE
+		direction = -1.0
+	else:
+		action_state = ACTION_STATE.IDLE
+		if hand_state == HAND_STATE.HODING:
+			hand_state = HAND_STATE.SWING
+			swing_duration_timer = SWING_DURATION
+			direction = -1.0
+	
+func attack_left_released():
+	attack_l_pressed = false
+	if attack_r_pressed == true:
+		action_state = ACTION_STATE.HOLD_CLOCKWISE
+		direction = 1.0
+	else:
+		action_state = ACTION_STATE.IDLE
+		if hand_state == HAND_STATE.HODING:
+			hand_state = HAND_STATE.SWING
+			swing_duration_timer = SWING_DURATION
+			direction = 1.0
 
 func _on_area_3d_body_entered(body):
 	var push_v = Vector3.ONE * PUSH_HORIZONTAL_V
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,6 +8,7 @@ var health = 3
 const JUMP_VELOCITY = 4.5
 @onready var mesh_instance_3d = $Hand/MeshInstance3D
 var player_radius = 5.0
+@export var player_id : int = 1
 @export var mouse_sens : float = 0.001
 @onready var character_model = $CharacterModel#Change after model is imported
 
@@ -23,34 +24,42 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var hand := $Hand
 @onready var collision_shape_3d = $CollisionShape3D
 
-func _enter_tree():
-	set_multiplayer_authority(str(name).to_int())
-	
-func _ready():
-	if not is_multiplayer_authority(): return
-	GameManager.set_player(self)
-	camera.current = is_multiplayer_authority()
-	backcam.current = is_multiplayer_authority()
-	forecam.current = is_multiplayer_authority()
+#func _enter_tree():
+	#set_multiplayer_authority(str(name).to_int())
+	#
+#func _ready():
+	#if not is_multiplayer_authority(): return
+	#GameManager.set_player(self)
+	#camera.current = is_multiplayer_authority()
+	#backcam.current = is_multiplayer_authority()
+	#forecam.current = is_multiplayer_authority()
 	
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack_r_%s" %[player_id]):
+		hand.attack_right_pressed()
+	if event.is_action_pressed("attack_l_%s" %[player_id]):
+		hand.attack_left_pressed()
 	
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN )
-	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	if event is InputEventMouseMotion:
-		if is_multiplayer_authority():
-			var mousePos = event.position;
-			var rayOrigin = camera.project_ray_origin(mousePos)
-			var rayEnd = rayOrigin + camera.project_ray_normal(mousePos) * 2000
-			var p = Plane(0, 1 , 0, position.y)
-			var point = p.intersects_ray(rayOrigin, rayEnd)
-			var direction = Vector2(point.z - position.z, point.x - position.x)
-			hand._set_target_dir(direction.normalized())
+	if event.is_action_released("attack_r_%s" %[player_id]):
+		hand.attack_right_released()
+	if event.is_action_released("attack_l_%s" %[player_id]):
+		hand.attack_left_released()
+	#if event is InputEventMouseButton:
+		#Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN )
+	#elif event.is_action_pressed("ui_cancel"):
+		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	#if event is InputEventMouseMotion:
+		#if is_multiplayer_authority():
+			#var mousePos = event.position;
+			#var rayOrigin = camera.project_ray_origin(mousePos)
+			#var rayEnd = rayOrigin + camera.project_ray_normal(mousePos) * 2000
+			#var p = Plane(0, 1 , 0, position.y)
+			#var point = p.intersects_ray(rayOrigin, rayEnd)
+			#var direction = Vector2(point.z - position.z, point.x - position.x)
+			#hand._set_target_dir(direction.normalized())
 
 func _physics_process(delta):
-	if is_multiplayer_authority():
+	#if is_multiplayer_authority():
 		# Add the gravity.
 		if not is_on_floor():
 			velocity.y -= gravity * delta
@@ -58,14 +67,15 @@ func _physics_process(delta):
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
 		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("jump_%s" %[player_id]) and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 		if Input.is_action_just_pressed("t_quit"):
 			$"../".exit_game(name.to_int())
 			get_tree().quit()
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
-		var input_dir = Input.get_vector("left", "right", "up", "down")
+		var input_dir = Input.get_vector("left_%s" %[player_id], "right_%s" %[player_id],
+		 								"up_%s" %[player_id], "down_%s" %[player_id])
 		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		
 		if direction:
