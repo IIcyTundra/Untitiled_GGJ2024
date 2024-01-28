c902918d4720b8968293249d5032cea424570997
Garry Chen <chen.garry81611@gmail.com>
2024-01-27 16:20:01 -0800

    Update hand swing to use angular speed

--- Scripts/Hand.gd
+++ Scripts/Hand.gd
@@ -1,35 +1,38 @@
 extends CharacterBody3D
 
-const acc = 5.0
-const max_speed = 4.5
-@onready var map = "res://Scenes/test_map.tscn"
-@onready var background_cam = $BaseCam/BackgroundPassContainer/BackgroundPass/BackgroundCam
-@onready var foreground_cam = $BaseCam/ForegroundPassContainer/ForegroundPass/ForegroundCam
+const TURN_SPEED_MAX := 100 # in radians/sec
+const TURN_SPEED_MIN := 10 # in radians/sec
+const TURN_ACC := 5 # in radians/sec
+const TURN_Thrash := 0.1 # in radians/sec
 
+var target_angle: float = 0.0
+var ang_speed: float = 0.0
+var direction: Vector2
+var is_clockwise = true
+var done = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	move_and_slide()
+func _physics_process(delta: float):
+	if done:
+		return
+	var old_direction = Vector2(cos(rotation.y), sin(rotation.y))
+	ang_speed += TURN_ACC * delta
+	ang_speed = clamp(ang_speed, TURN_SPEED_MIN, TURN_SPEED_MAX)
+	var diff = direction - old_direction
+	var new_direction = old_direction + diff * ang_speed * delta
+	rotation.y = new_direction.angle()
+	if diff.length() < TURN_Thrash:
+		done = true
 	
-#func _rotat_to(event, m_position):
-	#var spaceState = get_world_3d().direct_space_state
-	##position.angle_to(transform.basis)
-	#var mousePos = event.position
-	#var camera = get_viewport().get_camera_3d()
-	#var rayOrigin = camera.project_ray_origin(mousePos)
-	#var rayEnd = rayOrigin + camera.project_ray_normal(mousePos) * 2000
-	#var query = PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd, 0b0010)
-	#var rayArray = spaceState.intersect_ray(query)
-	#print("--------")
-	##if rayArray.has("position"):
-		##print(rayArray["position"])
-	#if foreground_cam:
-		#print(foreground_cam.position)
-	#print(rayOrigin)
-	#print(m_position)
-	#print(rayEnd)
-	#print(camera.project_ray_normal(mousePos))
+func _set_target_dir(dir):
+	direction = dir
+	var old_direction_p = Vector2(-sin(rotation.y), cos(rotation.y))
+	var new_is_clockwise = direction.dot(old_direction_p) < 0
+	if new_is_clockwise != is_clockwise:
+		ang_speed = 0
+	is_clockwise = new_is_clockwise
+	done = false
+
 	
