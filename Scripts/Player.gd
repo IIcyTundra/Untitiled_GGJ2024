cc61174713370226744f9ee22d83214514e45175
Jay Simmons <79724024+IIcyTundra@users.noreply.github.com>
2024-01-28 17:55:16 -0600

    reverted controls

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -44,7 +44,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump_1") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
