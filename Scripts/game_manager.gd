170d64559ec8ebd68518e912f1dae1f5f5403487
Jay Simmons <79724024+IIcyTundra@users.noreply.github.com>
2024-01-28 18:06:20 -0600

    Global func changes

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,20 +1,40 @@
 extends Node
 
-var player:Node
+var player1:Node
+var player2:Node
 var camera:Camera3D
 
 signal round_started
 signal round_ended
 
 
-func set_player(player_node):
-	player = player_node
-	print(player)
+func set_player1(player_node):
+	player1 = player_node
+	print(player1)
+
+func set_player2(player_node):
+	player2 = player_node
+	print(player2)
 
 func set_camera(camera_node):
 	camera = camera_node
 	print(camera)
+
+
+
+# Called when the player loses health
+func lose_health(health : int):
+	health -= 1
+	health = clamp(health, 0, 3)  # Ensure health doesn't go below 0 or above MAX_HEALTH
+	return health
 	
-	
+	#if health <= 0:
+		#die()
+
+
+func win_game():
+	# You can implement additional logic here, such as showing a victory screen, playing a win animation, etc.
+	print("Player has won the game!")
+
 
 
